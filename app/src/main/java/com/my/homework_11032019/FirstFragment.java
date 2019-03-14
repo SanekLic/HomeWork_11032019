9a92ec711c35333efc8b5b2a44d8560ed1f26347
sanek <saneklic@gmail.com>
2019-03-14 14:45:25 +0300

    complete

---
+++ app/src/main/java/com/my/homework_11032019/FirstFragment.java
@@ -0,0 +1,20 @@
+package com.my.homework_11032019;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    }
+}
